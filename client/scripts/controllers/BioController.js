bff1624f78dee7a3d93ba757931a45a139ebbf15
Anisa <anisa.ali7@gmail.com>
2017-04-28 17:12:38 -0500

    adding new bios to list - can't view indidual bio or edit though :/

--- client/scripts/controllers/BioController.js
+++ client/scripts/controllers/BioController.js
@@ -1,13 +1,14 @@
 /*jshint esversion: 6 */
 rootsApp.controller('BioController', ['$scope', '$http', 'UserService', function($scope, $http, UserService){
 
-UserService.getBio();
 //Get User Full name
 $scope.user = UserService.userObject;
 $scope.logout = UserService.logout;
+
 //Bio object from factory
 $scope.bioObject = UserService.bioObject;
 
+//update an existing bio
 $scope.update = UserService.updateBio;
 
 //create new bio is not one already
