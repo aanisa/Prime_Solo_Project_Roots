522d20b0b2e185951852ed0b084077520552bd79
Anisa <anisa.ali7@gmail.com>
2017-04-29 15:14:29 -0500

    additional bio data for early life and adult life

--- client/scripts/controllers/BioController.js
+++ client/scripts/controllers/BioController.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
 rootsApp.controller('BioController', ['$scope', '$http', 'UserService', function($scope, $http, UserService){
+UserService.getBio();
 
 //Get User Full name
 $scope.user = UserService.userObject;
@@ -19,5 +20,5 @@ $scope.onePerson = UserService.onePerson;
 
 $scope.viewBio = UserService.viewBio;
 
-UserService.getBio();
+
 }]);
